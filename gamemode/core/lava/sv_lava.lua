a529710ae15f964107607462c9d8ce9772ef50f6
Bull <thebull2140@gmail.com>
2017-06-18 12:07:43 -0400

    code cleanup

--- gamemode/core/lava/sv_lava.lua
+++ gamemode/core/lava/sv_lava.lua
@@ -1,35 +1,37 @@
-
+local SetGlobalFloat = SetGlobalFloat
+local Rounds = Rounds
 local Lava = Lava
+local Values = Values
+local FrameTime = FrameTime
 
 hook.Add("Think", "LavaSync", function()
-	if Lava.CurrentLevel ~= GetGlobalFloat("$lavalev", -10000 ) then
-		SetGlobalFloat( "$lavalev", Lava.CurrentLevel )
+	if Lava.CurrentLevel ~= GetGlobalFloat("$lavalev", -10000) then
+		SetGlobalFloat("$lavalev", Lava.CurrentLevel)
 	end
 end)
 
 hook.Add("Think", "LavaMain", function()
 	if Rounds.CurrentState == "Preround" then
-	--	Rounds.Start()
+		--
 	elseif Rounds.CurrentState == "Started" then
 		local max = 100000
-		for Player in Values( player.GetAll() ) do
+		for Player in Values(player.GetAll()) do
 			if not Player:Alive() then continue end
-			max = max:min( Player:GetPos().z - Lava.GetLevel() )
+			max = max:min(Player:GetPos().z - Lava.GetLevel())
 		end
+
 		if max == 100000 then
 			max = 1
 		end
 
-		Lava.ShiftLevel( (FrameTime()*max/20):Clamp( FrameTime()*4, FrameTime()*10 ) )
+		Lava.ShiftLevel((FrameTime() * max / 50))
 	elseif Rounds.CurrentState == "Ended" then
-
+		Lava.ShiftLevel(-FrameTime() * 5)
 	end
 end)
 
-hook.Add("PlayerTick", "LavaHurt", function( Player )
+hook.Add("PlayerTick", "LavaHurt", function(Player)
 	if Player:GetPos().z <= Lava.CurrentLevel then
-		Player:Ignite( 0.1, 0 )
+		Player:Ignite(0.1, 0)
 	end
 end)
---[[ 
-Lava.SetLevel( Entity(1):GetPos().z )--]] 
