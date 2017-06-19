de6adfe105aca6bd65ad3b46fa66712d324e9b32
Bull <thebull2140@gmail.com>
2017-06-18 20:29:47 -0400

    objectively better skybox fog

--- gamemode/core/environment/cl_fogsky.lua
+++ gamemode/core/environment/cl_fogsky.lua
@@ -2,18 +2,18 @@ local render = render
 local MATERIAL_FOG_LINEAR = MATERIAL_FOG_LINEAR
 local Config = GM.GetConfig()
 
-hook.Add("SetupWorldFog", "Fog", function()
-	if not Config.GetMapEffects() then return end
+local RenderFog = function(f)
 	render.FogStart(5)
 	render.FogColor(255, 128, 0)
 	render.FogMode(MATERIAL_FOG_LINEAR)
 	render.FogMaxDensity(0.9)
-	render.FogEnd(1000)
-
+	render.FogEnd(1500 * ( f or 1 ) )
 	return true
-end)
+end
 
-hook.Add("PostDrawSkyBox", "FogSkyUnity", function()
+hook.Add("SetupWorldFog", "WorldFog", RenderFog)
+hook.Add("SetupSkyboxFog", "SkyboxFog", RenderFog)
+hook.Add("PostDraw2DSkyBox", "FogSkyUnity", function()
 	if not Config.GetMapEffects() then return end
 	render.Clear(255, 128 - 75, 0, 0, false, true)
 end)
