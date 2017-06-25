ed7cdb855ab3c08ef75b6ac41062cf133b7c2cc5
Bull <thebull2140@gmail.com>
2017-06-25 14:49:18 -0400

    baseclass resets lavawalker values

--- gamemode/core/player/classes/default.lua
+++ gamemode/core/player/classes/default.lua
@@ -10,9 +10,12 @@ PLAYER.StartHealth = 5
 PLAYER.TeammateNoCollide = false
 
 function PLAYER:Spawn()
+	self.Player:SetModelScale( 1, 0 )
 	self.Player:SetModel(("models/player/Group01/male_0<??>.mdl"):fill(math.random(1, 9)))
 	self.Player:SetPlayerColor(Vector((1):random(255) / 255, (1):random(255) / 255, (1):random(255) / 255))
 	self.Player:SetupHands()
+	self.Player:SetViewOffset( Vector( 0, 0, 64 ))
+	self.Player:SetViewOffsetDucked( Vector( 0, 0, 28 ))
 end
 
 player_manager.RegisterClass("lava_default", PLAYER, "player_default" )
