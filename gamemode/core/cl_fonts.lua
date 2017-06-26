a8b38e7c3f1482160d1a564ea5e465df7a2a7786
Bull <thebull2140@gmail.com>
2017-06-26 00:53:44 -0400

    sumfontz

--- gamemode/core/cl_fonts.lua
+++ gamemode/core/cl_fonts.lua
@@ -12,3 +12,20 @@ surface.CreateFont("lava_round_timer", {
 	weight = 800,
 	outline = false
 })
+
+
+surface.CreateFont("lava_abilities_header", {
+	font = "Coolvetica",
+	size = ScrH() / 15,
+	antialias = true,
+	weight = 800,
+	outline = false
+})
+
+surface.CreateFont("lava_abilities_desc", {
+	font = "Roboto Bold",
+	size = ScrH() / 30,
+	antialias = true,
+	weight = 200,
+	outline = false
+})
