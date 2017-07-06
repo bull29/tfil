b316c469bb71a202ebd808e53e39a3bf13dfc66a
Bull <thebull2140@gmail.com>
2017-07-05 21:42:15 -0400

    more hookzz

--- gamemode/core/player/sv_player.lua
+++ gamemode/core/player/sv_player.lua
@@ -3,6 +3,8 @@ function GM:PlayerSpawn(Player)
 	hook.Call( "ChoosePlayerClass", GAMEMODE, Player )
 	player_manager.OnPlayerSpawn( Player )
 	player_manager.RunClass( Player, "Spawn" )
+	Player:Give("lava_fists")
+	hook.Call("Lava.PostPlayerSpawn", nil, Player )
 end
 
 function GM:ChoosePlayerClass( Player )
