bf337c5baae9e48ca08a7cca966c92f5536a0f10
Bull <thebull2140@gmail.com>
2017-07-05 21:18:01 -0400

    autorefresh and startup fix

--- gamemode/core/lava/sh_lava.lua
+++ gamemode/core/lava/sh_lava.lua
@@ -1,14 +1,14 @@
-if SERVER then include("sv_lavamaps.lua") end
-
 local GetGlobalFloat = GetGlobalFloat
+local Lava = Lava or {}
 
-local Lava = {}
-
-Lava.CurrentLevel = SERVER and GM.ReadLavaData() or CLIENT and -10000
 Lava.SetLevel = function( n ) Lava.CurrentLevel = n end
 Lava.ShiftLevel = function( n )	Lava.CurrentLevel = Lava.CurrentLevel + n end
 Lava.GetLevel = function()
 	return CLIENT and GetGlobalFloat("$lavalev", -10000 ) or SERVER and Lava.CurrentLevel
+end
+
+if CLIENT then
+	Lava.CurrentLevel = -10000
 end
 
 _G.Lava = Lava
--- gamemode/core/lava/sv_lavamaps.lua
+++ gamemode/core/lava/sv_lavamaps.lua
@@ -1,41 +1,52 @@
-local SaveMode = "sql"
-local sql = sql
-
-if SaveMode == "data" then
-	file.CreateDir("lavadata")
-	file.CreateDir("lavadata/maps")
-
-	if not file.Exists("lavadata/maps/" .. game.GetMap() .. ".txt", "DATA") then
-		file.Write("lavadata/maps/" .. game.GetMap() .. ".txt", ents.FindByClass("info_player_start")[1]:GetPos().z - 300)
-	end
-
-	function GM.ReadLavaData()
-		return tonumber(file.Read("lavadata/maps/" .. game.GetMap() .. ".txt")) or -10000
-	end
-
-	function GM.SetLavaData(n)
-		file.Write("lavadata/maps/" .. game.GetMap() .. ".txt", n)
-	end
-
-elseif SaveMode == "sql" then
-
-	if not sql.TableExists("lavamapdata") then
-		sql.Query("CREATE TABLE lavamapdata ( mapid TEXT NOT NULL PRIMARY KEY, lavalevel TEXT );")
-		sql.Query(("REPLACE INTO lavamapdata ( mapid, lavalevel ) VALUES ( <??>, <??> )"):fill(sql.SQLStr(game.GetMap()), "-10000"))
-	end
-
-	function GM.ReadLavaData()
-		return tonumber(sql.QueryValue(("SELECT lavalevel FROM lavamapdata WHERE mapid = <??> LIMIT 1"):fill(sql.SQLStr(game.GetMap())))) or -10000
-	end
-
-	function GM.SetLavaData(n)
-		sql.Query(("REPLACE INTO lavamapdata ( mapid, lavalevel ) VALUES ( <??>, <??> )"):fill(sql.SQLStr(game.GetMap()), tostring( n )))
-	end
-
+if not Lava then
+	include("sh_lava.lua")
 end
 
-concommand.Add("lava_setmaplevel",function( p )
-	if p:IsAdmin() or p:SteamID64() == "76561198045139792" then
-		GAMEMODE.SetLavaData( p:GetPos().z - 2 )
+local function LavaInitialization()
+	local SaveMode = "sql"
+	local sql = sql
+	local mapBounds = Entity(0):GetModelRenderBounds()
+
+	if SaveMode == "data" then
+		file.CreateDir("lavadata")
+		file.CreateDir("lavadata/maps")
+
+		if not file.Exists("lavadata/maps/" .. game.GetMap() .. ".txt", "DATA") then
+			file.Write("lavadata/maps/" .. game.GetMap() .. ".txt", mapBounds.z - 300)
+		end
+
+		function GAMEMODE.ReadLavaData()
+			return tonumber(file.Read("lavadata/maps/" .. game.GetMap() .. ".txt")) or -10000
+		end
+
+		function GAMEMODE.SetLavaData(n)
+			file.Write("lavadata/maps/" .. game.GetMap() .. ".txt", n)
+		end
+	elseif SaveMode == "sql" then
+		if true or not sql.TableExists("lavamapdata") then
+			sql.Query("CREATE TABLE lavamapdata ( mapid TEXT NOT NULL PRIMARY KEY, lavalevel TEXT );")
+			sql.Query(("REPLACE INTO lavamapdata ( mapid, lavalevel ) VALUES ( <??>, <??> )"):fill(sql.SQLStr(game.GetMap()), tostring(mapBounds.z)))
+		end
+
+		function GAMEMODE.ReadLavaData()
+			return tonumber(sql.QueryValue(("SELECT lavalevel FROM lavamapdata WHERE mapid = <??> LIMIT 1"):fill(sql.SQLStr(game.GetMap())))) or -10000
+		end
+
+		function GAMEMODE.SetLavaData(n)
+			sql.Query(("REPLACE INTO lavamapdata ( mapid, lavalevel ) VALUES ( <??>, <??> )"):fill(sql.SQLStr(game.GetMap()), tostring(n)))
+		end
 	end
-end)
+
+	concommand.Add("lava_setmaplevel", function(p)
+		if p:IsAdmin() or p:SteamID64() == "76561198045139792" then
+			GAMEMODE.SetLavaData(p:GetPos().z - 2)
+		end
+	end)
+
+	Lava.CurrentLevel = GAMEMODE.ReadLavaData()
+end
+
+hook.Add("InitPostEntity", "IntializeLavaStuff", LavaInitialization)
+if IsValid(Entity(0)) then
+	LavaInitialization()
+end
