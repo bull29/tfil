7bcca5ab9b40788b003896a4b1859726ab7d9905
Bull <thebull2140@gmail.com>
2017-07-05 21:38:02 -0400

    string lazyness

--- gamemode/libraries/string.lua
+++ gamemode/libraries/string.lua
@@ -1,11 +1,11 @@
-
-
-
-
 function string.fill( str, ... )
 	local args = { ... }
 	for i = 1, #args do
 		str = str:gsub("<%?%?>", args[ i ], 1)
 	end
 	return str
+end
+
+function string.JSONDecode( str )
+	return util.JSONToTable( str )
 end
