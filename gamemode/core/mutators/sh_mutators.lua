fdd5a60db89a539b8b6638e125f0cf114989d0a8
Bull <thebull2140@gmail.com>
2017-07-07 03:40:54 -0400

    mutator framework refinement

--- gamemode/core/mutators/sh_mutators.lua
+++ gamemode/core/mutators/sh_mutators.lua
@@ -1,36 +1,57 @@
 --[[
 	Round Mutators - Shit to make each and every round more interesting
 ]]
-Mutators = Mutators or {}
+local Mutators = Mutators or {}
 Mutators.Events = Mutators.Events or {}
 
 function Mutators.RegisterNewEvent(name, desc, startfunc, endfunc)
 	Mutators.Events[name] = {
-		["Name"] = name,
-		["Description"] = desc,
-		["StartFunction"] = startfunc,
-		["EndFunction"] = endfunc
+		["name"] = name,
+		["desc"] = desc,
+		["startfn"] = startfunc,
+		["endfn"] = endfunc
 	}
 end
 
 function Mutators.StartEvent(event)
+	if hook.Call("Lava.MutatorStart", nil, Mutators.Events[ event ] ) == false then return end
+
+	if not Mutators.Events[ event ] then return end
+
+	Mutators.Events[ event ].startfn()
+
 	if SERVER then
 		SetGlobalString("$activemutator", event)
 	end
 
-	if CLIENT then
-		GAMEMODE.CreateNotification(([[Round Mutator: <??>
+end
 
-				<??>]]):fill(event, Mutators.Events[event].Description), 5)
+function Mutators.EndEvent()
+	local tab = Mutators.Events[ GetGlobalString("$activemutator") ]
+	if not tab then return end
+
+	if tab.endfn then
+		tab.endfn()
 	end
+
+	SetGlobalString("$activemutator", "" )
+end
+
+function Mutators.IsActive( name )
+	if not name then return GetGlobalString("$activemutator") ~= "" end
+	return GetGlobalString("$activemutator") == name
 end
 
 hook.RunOnce("HUDPaint", function()
-	if GetGlobalString("$activemutator") ~= "" then
+	if Mutators.IsActive() then
 		Mutators.StartEvent(GetGlobalString("$activemutator"))
 	end
 end)
 
-hook.Add("PostRound", "EndMutator", function()
-	SetGlobalString("$activemutator", nil)
+hook.Add("Lava.PostRound", "EndMutator", function()
+	if GetGlobalString("$activemutator", "" ) ~= "" then
+		Mutators.EndEvent()
+	end
 end)
+
+_G.Mutators = Mutators
