0562d02f998a1c2a6f2d87ff2a6fd3b348a5642b
Bull <thebull2140@gmail.com>
2017-07-07 03:34:34 -0400

    thanks to zombine, unified lava rendering

--- gamemode/core/lava/cl_lava.lua
+++ gamemode/core/lava/cl_lava.lua
@@ -7,32 +7,59 @@ local CurTime = CurTime
 local math = math
 local drawOverlay = DrawMaterialOverlay
 local drawColor = DrawBloom
+local GetGlobalVector = GetGlobalVector
 local LocalPlayer = LocalPlayer
 local surface = surface
 local v = Vector()
 local LavaTexture = "http://i.imgur.com/swJIriB.jpg"
 local SmoothLevel = -1000
 local MapScale = 1
+local SkyboxScale = 1
+local MapBounds = Vector()
+local ClipTab = {}
+
+local dirs = {
+	left = Vector(-1, 0, 0),
+	frwd = Vector(0, -1, 0)
+}
+
 local function GetMapBounds()
 	local a, b = Entity(0):GetModelRenderBounds()
 	a.z, b.z = 0, 0
-	return a:Distance( b )
+
+	return a:Distance(b)
 end
+
+hook.RunOnce("SetupSkyboxFog", function(Scale)
+	SkyboxScale = 1 / Scale
+end)
 
 hook.RunOnce("HUDPaint", function()
 	MapScale = GetMapBounds()
+	MapBounds = Entity(0):GetModelRenderBounds()
+	ClipTab = { {dirs.left, MapBounds.x}, {-dirs.left, MapBounds.x}, {dirs.frwd, MapBounds.y}, {dirs.frwd, MapBounds.y} }
 end)
 
 hook.Add("PostDrawTranslucentRenderables", "DrawLava", function(a, b)
-	if b then return end
 	SmoothLevel = SmoothLevel:lerp(Lava.GetLevel())
+	local LavaLevel = v:SetZ(SmoothLevel)
 
-	cam.Wrap3D2D(function()
+	render.Clip( ClipTab, function()
 		surface.SetDrawColor(255, 255, 255)
 		surface.SetMaterial(draw.fetch_asset(LavaTexture, "noclamp"))
-		surface.DrawTexturedRectUV(-MapScale/2, -MapScale/2, MapScale, MapScale, 0, 0, 10, 10)
-	end, v:SetZ(SmoothLevel), Angle(0, CurTime() / 3, 0), 1)
+
+		if not b then
+			cam.Wrap3D2D(function()
+				surface.DrawTexturedRectUV(-MapScale / 2, -MapScale / 2, MapScale, MapScale, 0, 0, 10, 10)
+			end, LavaLevel, Angle(0, CurTime() / 3, 0), 1)
+		end
+
+		cam.Wrap3D2D(function()
+			surface.DrawTexturedRectUV(-MapScale / 2, -MapScale / 2, MapScale, MapScale, 0, 0, 10 * SkyboxScale, 10 * SkyboxScale)
+		end, GetGlobalVector("$skycampos") + (LavaLevel / SkyboxScale), Angle(0, CurTime() / 3, 0), 1)
+	end)
 end)
+
 
 hook.Add("RenderScreenspaceEffects", "DrawLavaOverlay", function()
 	if hook.Call("Lava.ShouldRenderDamageOverlay") == false then return end
