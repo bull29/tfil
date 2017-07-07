2fe6fc6dee28066814559c3431732a4add4c4650
Bull <thebull2140@gmail.com>
2017-07-07 03:33:15 -0400

    slightly more ominous fog

--- gamemode/core/environment/cl_fogsky.lua
+++ gamemode/core/environment/cl_fogsky.lua
@@ -6,7 +6,8 @@ local bTab = { [ "$pp_colour_addr" ] = 0.02, [ "$pp_colour_addg" ] = 0.02, [ "$p
 local fDensity = 0
 local RenderFog = function(f)
 	if not Config.GetMapEffects() then fDensity = fDensity:lerp( 0 ) return end
-	fDensity = fDensity:lerp( 0.9, FrameTime()/3 )
+	fDensity = fDensity:lerp( 1, FrameTime()/3 ):min( 0.95 )
+	--fDensity = 1
 	render.FogStart(5)
 	render.FogColor(255, 128, 0)
 	render.FogMode(MATERIAL_FOG_LINEAR)
@@ -18,7 +19,7 @@ hook.Add("SetupWorldFog", "WorldFog", RenderFog)
 hook.Add("SetupSkyboxFog", "SkyboxFog", RenderFog)
 hook.Add("PostDraw2DSkyBox", "FogSkyUnity", function()
 	if not Config.GetMapEffects() then return end
-	render.Clear(283 * fDensity, 59 * fDensity, 0, 0, false, true)
+	render.Clear(255 * fDensity, 55 * fDensity, 0, 0, false, true)
 end)
 hook.Add("RenderScreenspaceEffects","LavaColorModify",function()
 	if not Config.GetMapEffects() then return end
