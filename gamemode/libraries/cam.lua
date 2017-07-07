dc08e8c32e05d57648ce0f3693a1ed77b0bf7d60
Bull <thebull2140@gmail.com>
2017-07-07 03:30:14 -0400

    some call it abstraction, some call it retardation

--- gamemode/libraries/cam.lua
+++ gamemode/libraries/cam.lua
@@ -1,6 +1,7 @@
 if SERVER then return end
 
 local cam = cam
+local render = render
 
 function cam.Wrap2D( func, ... )
 	cam.Start2D( ... )
@@ -18,4 +19,16 @@ function cam.Wrap3D2D( func, ... )
 	cam.Start3D2D( ... )
 		func()
 	cam.End3D2D()
+end
+
+function render.Clip( tab, func )
+	render.EnableClipping( true )
+	for i = 1, #tab do
+		render.PushCustomClipPlane( tab[i][1], tab[i][2])
+	end
+	func()
+	for i = 1, #tab do
+		render.PopCustomClipPlane()
+	end
+	render.EnableClipping(false)
 end
