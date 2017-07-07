26021fefad9579d43ec1c5ed8ad1faf48d9bb679
Bull <thebull2140@gmail.com>
2017-07-07 03:39:26 -0400

    shoo gcc

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,9 +1,1 @@
 include("include.lua")
-
-function GlobalCustomColor( returnascolor )
-	local x = LocalPlayer():GetPlayerColor()
-	if returnascolor then
-		return Color( x.r*255, x.g*255, x.b*255 )
-	end
-	return x.r*255, x.g*255, x.b*255, 255
-end
