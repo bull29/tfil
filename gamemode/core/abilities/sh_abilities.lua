21f365e4365dde7ec85cdc161a2d3379daf54a87
Bull <thebull2140@gmail.com>
2017-07-10 18:36:09 -0400

    player.GetAbility

--- gamemode/core/abilities/sh_abilities.lua
+++ gamemode/core/abilities/sh_abilities.lua
@@ -10,4 +10,8 @@ debug.getregistry().Player.HasAbility = function( self, ability )
 	return SERVER and self.Ability == ability or CLIENT and self:GetNW2String("$ability") == ability
 end
 
+debug.getregistry().Player.GetAbility = function( self )
+	return SERVER and self.Ability or CLIENT and self:GetNW2String("$ability")
+end
+
 _G.Abilities = Abilities
