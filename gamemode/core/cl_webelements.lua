9e1bb3ca3214ee038e2bd17e1d803816821fa12c
Bull <thebull2140@gmail.com>
2017-07-10 18:33:20 -0400

    online piracy

---
+++ gamemode/core/cl_webelements.lua
@@ -0,0 +1,9 @@
+local wE = {}
+
+wE.CircleOutline	= "http://i.imgur.com/WhK77op.png"
+wE.Circle 	  		= "http://i.imgur.com/YmbEJWD.png"
+wE.Lava 	  		= "http://i.imgur.com/swJIriB.jpg"
+wE.BandedCircle		= "http://i.imgur.com/OQpzdFx.png"
+wE.HeartOutline		= "http://i.imgur.com/OFaYwW7.png"
+
+_G.WebElements = wE
