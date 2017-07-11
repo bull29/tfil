4c8b859412e6d6a9a84823d52669493726b1c7bd
Bull <thebull2140@gmail.com>
2017-07-11 12:58:47 -0400

    lava fix

--- gamemode/core/lava/sh_lava.lua
+++ gamemode/core/lava/sh_lava.lua
@@ -3,12 +3,11 @@ local Lava = Lava or {}
 
 Lava.SetLevel = function( n ) Lava.CurrentLevel = n end
 Lava.ShiftLevel = function( n )	Lava.CurrentLevel = Lava.CurrentLevel + n end
+Lava.StartingLevel = false
+Lava.CurrentLevel = -32768
 Lava.GetLevel = function()
-	return CLIENT and GetGlobalFloat("$lavalev", -10000 ) or SERVER and Lava.CurrentLevel
+	return CLIENT and GetGlobalFloat("$lavalev", -32768 ) or SERVER and Lava.CurrentLevel
 end
 
-if CLIENT then
-	Lava.CurrentLevel = -10000
-end
 
 _G.Lava = Lava
