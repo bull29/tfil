cf974640c8c19d1fb7b823fe2d799442fd5bb5f9
Bull <thebull2140@gmail.com>
2017-07-15 14:24:44 -0400

    adjusted orange cock

--- gamemode/core/environment/cl_fogsky.lua
+++ gamemode/core/environment/cl_fogsky.lua
@@ -2,19 +2,19 @@ local render = render
 local MATERIAL_FOG_LINEAR = MATERIAL_FOG_LINEAR
 local Config = GM.GetConfig()
 local dc = DrawColorModify
-local bTab = { [ "$pp_colour_addr" ] = 0.02, [ "$pp_colour_addg" ] = 0.02, [ "$pp_colour_addb" ] = 0, [ "$pp_colour_brightness" ] = 0, [ "$pp_colour_contrast" ] = 1, [ "$pp_colour_colour" ] = 3, [ "$pp_colour_mulr" ] = 0, [ "$pp_colour_mulg" ] = 0.02, [ "$pp_colour_mulb" ] = 0 }
+local bTab = { [ "$pp_colour_addr" ] = 0.02, [ "$pp_colour_addg" ] = 0.02, [ "$pp_colour_addb" ] = 0, [ "$pp_colour_brightness" ] = 0, [ "$pp_colour_contrast" ] = 1, [ "$pp_colour_colour" ] = 2, [ "$pp_colour_mulr" ] = 0, [ "$pp_colour_mulg" ] = 0.02, [ "$pp_colour_mulb" ] = 0 }
 local fDensity = 0
 local RenderFog = function(f)
 	if not Config.GetMapEffects() then fDensity = fDensity:lerp( 0 ) return end
-	fDensity = fDensity:lerp( 1, FrameTime()/3 ):min( 0.95 )
-	--fDensity = 1
+	fDensity = fDensity:lerp( 0.9, FrameTime()/3 )
 	render.FogStart(5)
 	render.FogColor(255, 128, 0)
 	render.FogMode(MATERIAL_FOG_LINEAR)
 	render.FogMaxDensity(fDensity)
-	render.FogEnd( (3000 - (1500*fDensity) )* ( f or 1 ) )
+	render.FogEnd( (5000 - (1500*fDensity) )* ( f or 1 ) )
 	return true
 end
+
 hook.Add("SetupWorldFog", "WorldFog", RenderFog)
 hook.Add("SetupSkyboxFog", "SkyboxFog", RenderFog)
 hook.Add("PostDraw2DSkyBox", "FogSkyUnity", function()
@@ -26,5 +26,5 @@ hook.Add("RenderScreenspaceEffects","LavaColorModify",function()
 	dc( bTab )
 end)
 hook.Add("Lava.PostRound", "ResetFoglerp", function()
-	fDensity = fDensity:lerp( 0 )
+	fDensity = 0
 end)
