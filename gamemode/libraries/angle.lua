0d3692fa6ab7bfc552d25fff7177fead037903b5
Bull <bull29@users.noreply.github.com>
2017-07-15 14:16:45 -0400

    lil fixerino

--- gamemode/libraries/angle.lua
+++ gamemode/libraries/angle.lua
@@ -4,14 +4,17 @@ local Angle = Angle
 function Ang:SetPitch(n, alter)
 	if not alter then return Angle(n, self.y, self.r) end
 	self.p = n
+	return self
 end
 
 function Ang:SetYaw(n, alter)
 	if not alter then return Angle(self.p, n, self.r) end
 	self.y = n
+	return self
 end
 
 function Ang:SetRoll(n, alter)
 	if not alter then return Angle(self.p, self.y, n) end
 	self.r = n
+	return self
 end
