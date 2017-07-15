fd07bf20d24fcf6965932f6956c020f5e79f0cb1
Bull <thebull2140@gmail.com>
2017-07-15 14:21:12 -0400

    math.inrange

--- gamemode/libraries/math.lua
+++ gamemode/libraries/math.lua
@@ -11,6 +11,10 @@ function math.NormalizedInt( n, onNeg, onPos )
 	return n == 0 and 0 or n < 0 and (onNeg or -1) or (onPos or 1)
 end
 
+function math.inrange( n, min, max )
+	return n >= min and n <= max
+end
+
 debug.setmetatable(-1, {
 	__index = math
 })
