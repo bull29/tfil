435c8a7bb57c016c3d370e17d29b652e141edf6a
Bull <thebull2140@gmail.com>
2017-07-20 02:51:41 -0400

    quality of life

--- gamemode/libraries/entity.lua
+++ gamemode/libraries/entity.lua
@@ -8,4 +8,20 @@ function ENT:SetProperVar( type, key, value, legacy )
 	end
 end
 
-if CLIENT then return end
+function ENT:GetPhysicsBone( name )
+	local c = self:TranslateBoneToPhysBone( self:LookupBone( name ) )
+	if not c then return end
+	return self:GetPhysicsObjectNum( c )
+end
+
+function ENT:DumpSequences()
+	for i = 1, self:GetSequenceCount() do
+		print( i, self:GetSequenceName( i ), self:GetSequenceActivity( i ))
+	end
+end
+
+function ENT:DumpBones()
+	for i = 1, self:GetBoneCount() do
+		print( i, self:GetBoneName( i ) )
+	end
+end
