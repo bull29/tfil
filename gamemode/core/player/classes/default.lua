15793ddf14b1a94e6872685d2f4dc08efe811f69
Bull <thebull2140@gmail.com>
2017-07-20 02:50:35 -0400

    default class update

--- gamemode/core/player/classes/default.lua
+++ gamemode/core/player/classes/default.lua
@@ -5,13 +5,15 @@ PLAYER.WalkSpeed = 150
 PLAYER.RunSpeed = 250
 PLAYER.CrouchedWalkSpeed = 0.2
 PLAYER.JumpPower = 250
-PLAYER.MaxHealth = 5
-PLAYER.StartHealth = 5
+PLAYER.MaxHealth = 100
+PLAYER.StartHealth = 100
 PLAYER.TeammateNoCollide = false
 
 function PLAYER:Spawn()
+	self.Player:SetDuckSpeed( 0.2 )
+	self.Player:SetUnDuckSpeed( 0.1 )
 	self.Player:SetModelScale( 1, 0 )
-	self.Player:SetModel(("models/player/Group01/male_0<??>.mdl"):fill(math.random(1, 9)))
+	self.Player:SetModel(("models/player/Group01/male_0${1}.mdl"):fill(math.random(1, 9)))
 	self.Player:SetPlayerColor(Vector((1):random(255) / 255, (1):random(255) / 255, (1):random(255) / 255))
 	self.Player:SetupHands()
 	self.Player:SetViewOffset( Vector( 0, 0, 64 ))
