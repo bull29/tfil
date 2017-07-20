284b49a42e7b70391063bdaf26c7e0ea043b56b8
Bull <thebull2140@gmail.com>
2017-07-20 01:37:20 -0400

    buffed dexterity

--- gamemode/core/abilities/dexterity/dexterity.lua
+++ gamemode/core/abilities/dexterity/dexterity.lua
@@ -1,15 +1,15 @@
 hook.Add("GetFallDamage", "LessenFallDamageDexitrity", function(Player, Speed )
-	if Player:HasAbility("Dexterity") then return ( 10 ):max( math.ceil( 0.2418*Speed - 141.75 )*1.5 ) end
+	if Player:HasAbility("Dexterity") then return ( 10 ):max( math.ceil( 0.2418*Speed - 141.75 )*1.5 )/3 end
 end)
 
 hook.Add("Lava.PostPlayerSpawn", "AddDexterity", function( Player )
 	if Player:HasAbility("Dexterity") then
-		Player:SetRunSpeed( 350 )
-		Player:SetWalkSpeed( 250 )
-		Player:SetJumpPower( 350 )
+		Player:SetRunSpeed( 400 )
+		Player:SetWalkSpeed( 300 )
+		Player:SetJumpPower( 400 )
 	end
 end)
 
 Abilities.Register("Dexterity", [[Your running speed
 	and jumping power is doubled,
-	and you take 25% less fall damage.]], CLIENT and Emoji.Get( 2106 ) )
+	and you take ~70% less fall damage.]], CLIENT and Emoji.Get( 2106 ) )
