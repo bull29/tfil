cf8e110521fa321ab52863fb198aea9e773360fd
Bull <thebull2140@gmail.com>
2017-07-21 11:29:24 -0400

    oh this life I cannot change

--- gamemode/libraries/misc.lua
+++ gamemode/libraries/misc.lua
@@ -1,6 +1,12 @@
 local MV = debug.getregistry().CMoveData
 local bit = bit
+local timer = timer
+local FrameTime = FrameTime
 
 function MV:RemoveKey( keys )
 	self:SetButtons( bit.band( self:GetButtons(), bit.bnot( keys ) ) )
+end
+
+function FrameDelay( func )
+	timer.Simple( FrameTime()*2, func )
 end
