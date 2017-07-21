2ce0e5956ae3cd1aeca330679366913bf131d504
Bull <thebull2140@gmail.com>
2017-07-21 17:26:51 -0400

    Removed unused fTick, Streamlined fall damage

--- gamemode/core/base.lua
+++ gamemode/core/base.lua
@@ -1,20 +1,13 @@
---[[-------------------------------------------------------------------------
- 		FullTick - For something you'd rather not use a timer for
----------------------------------------------------------------------------]]
-
-GM.f_NextTick = CurTime()
-
-function GM:Tick()
-	if self.f_NextTick <= CurTime() then
-		self.f_NextTick = CurTime() + 1
-		hook.Call( "fTick", GAMEMODE )
-	end
-end
+local math = math
 
 function GM:OnGamemodeLoaded()
 	GAMEMODE, GM = gmod.GetGamemode(), gmod.GetGamemode()
 end
 
-function GM:GetFallDamage( Player, Speed )
+function CalculateBaseFallDamage( Speed )
 	return math.max( 15, math.ceil( 0.2418*Speed - 141.75 )*2 )
+end
+
+function GM:GetFallDamage( Player, Speed )
+	return CalculateBaseFallDamage( Speed )
 end
