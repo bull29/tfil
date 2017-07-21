0cbda49ecf34c9e930c5343aa53c43004877ddf6
Bull <thebull2140@gmail.com>
2017-07-21 03:12:18 -0400

    more fontz

--- gamemode/core/cl_fonts.lua
+++ gamemode/core/cl_fonts.lua
@@ -40,23 +40,37 @@ surface.CreateFont("lava_voice_panel", {
 surface.CreateFont("lava_hud_panel", {
 	font = "Coolvetica",
 	size = ScrH() / 32,
-	antialias = true,
 	weight = 200,
 })
 
 surface.CreateFont("lava_altimeter_panel", {
 	font = "Roboto Condensed",
 	size = ScrH() / 40,
-	antialias = true,
 	weight = 100,
 })
-
-
 
 surface.CreateFont("lava_context_text_title", {
 	font = "Roboto Bold",
 	size = ScrH() / 15,
-	antialias = true,
+	weight = 100,
+})
+
+surface.CreateFont("lava_spectate_helper", {
+	font = "Roboto Bold",
+	size = ScrH() / 50,
+	weight = 100,
+})
+
+surface.CreateFont("lava_spectate_title", {
+	font = "Roboto Bold",
+	size = ScrH() / 25,
+	weight = 100,
+})
+
+
+surface.CreateFont("lava_spectate_hp", {
+	font = "Roboto Bold",
+	size = ScrH() / 50,
 	weight = 100,
 })
 
