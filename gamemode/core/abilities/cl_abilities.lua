b4955de26744b19534339c8323d2791649988615
Bull <thebull2140@gmail.com>
2017-07-21 00:57:48 -0400

    distance check - oversight

--- gamemode/core/abilities/cl_abilities.lua
+++ gamemode/core/abilities/cl_abilities.lua
@@ -1,62 +1,79 @@
-
-
 local draw = draw
-local dark = Color( 70, 70, 70 )
+local dark = Color(70, 70, 70)
 local pColor = pColor
 
-hook.RunOnce("HUDPaint", function()
-	local x = InitializePanel("LavaAbiltiesSelector")
-	x:SetSize( ScrW()/4, ScrH()*0.87 )
-	x:SetTitle( "Abilities" )
+local function CreateAbilitiesPanel()
+	local x = InitializePanel("LavaAbiltiesSelector", "DPanel")
+	x:SetSize(ScrW() / 4, ScrH() * 0.87)
 	x:MakePopup()
 	x:Center()
-	x.Paint = function( s, w, h )
-		draw.RoundedBox( 0, 0, 0, w, h, dark  )
+
+	x.Paint = function(s, w, h)
+		draw.RoundedBox(0, 0, 0, w, h, pColor() - 150 )
 	end
 
-	local m = x:Add( "DPanel" )
-	m:Dock( FILL )
-	m.Paint = function( s, w, h )
-		draw.RoundedBox( 0, 0, 0, w, h, pColor() )
+	local m = x:Add("DPanel")
+	m:Dock(FILL)
+	m:DockMargin( WebElements.Edge, WebElements.Edge, WebElements.Edge, WebElements.Edge)
+
+	m.Paint = function(s, w, h)
+		draw.RoundedBox(0, 0, 0, w, h, pColor())
 	end
 
 	local s = m:Add"DScrollPanel"
-	s:Dock( TOP )
-	s:SetHeight( ScrH()/2 )
-
+	s:Dock(TOP)
+	s:SetHeight(ScrH() / 2)
 	local a = s:Add("DIconLayout")
-	a:Dock( FILL )
-	a:SetSpaceX( 1 )
-	a:SetSpaceY( 1 )
-
+	a:Dock(FILL)
+	a:SetSpaceX(1)
+	a:SetSpaceY(1)
 	local d = m:Add("DPanel")
-	d:Dock( BOTTOM )
+	d:Dock(BOTTOM)
 	d.Desc = ""
 	d.Header = ""
-	d:SetHeight( ScrH()/3 )
-	d.Paint = function( s, w, h )
-		draw.RoundedBox( 0, 0, 0, w, h, dark:Alpha( 225 ) )
-		draw.SimpleText( s.Header, "lava_abilities_header", ScrH()/100, ScrH()/100 )
-		draw.DrawText( s.Desc, "lava_abilities_desc", ScrH()/100, ScrH()/100 + ScrH()/15 )
+	d:SetHeight(ScrH() / 3)
+
+	d.Paint = function(s, w, h)
+		draw.RoundedBox(0, 0, 0, w, h, dark:Alpha(225))
+		draw.SimpleText(s.Header, "lava_abilities_header", ScrH() / 100, ScrH() / 100)
+		draw.DrawText(s.Desc, "lava_abilities_desc", ScrH() / 100, ScrH() / 100 + ScrH() / 15)
 	end
 
 	for k, v in pairs(Abilities.Skills) do
 		local c = a:Add("DButton")
-		c:SetSize( ScrH()/10 , ScrH()/10 )
+		c:SetSize(ScrH() / 10, ScrH() / 10)
 		c:SetText("")
-		c.Paint = function( s, w, h )
-			draw.RoundedBox( 0, 0, 0, w, h, dark:Alpha( s.Hovered and 200 or 150 ) )
-			draw.WebImage( v[2], h*0.1, h*0.1, w - h*0.2, h - h*0.2 )
+		c:GenerateColorShift( "HoverVar", dark:Alpha( 50 ), dark:Alpha( 200 ) + 50, 255 * 3 )
+		c.Paint = function(s, w, h)
+			draw.RoundedBox(0, 0, 0, w, h, s.HoverVar)
+			draw.WebImage(v[2], h * 0.1, h * 0.1, w - h * 0.2, h - h * 0.2)
+
 			if s.Hovered then
 				d.Header = k
-				d.Desc = v[ 1 ]:gsub("\t", "")
+				d.Desc = v[1]:gsub("\t", "")
 			end
 		end
+
 		c.DoClick = function()
-			chat.AddText( pColor(), "You've selected the " .. k .. " ability! Your selection will take place the next time you spawn." )
+			chat.AddText(pColor(), "You've equipped the " .. k .. " ability!" .. ( Rounds.IsState("Preround") and "" or " Your selection will take place the next time you spawn." ) )
 			net.Start("Lava.SelectAbility")
-			net.WriteString( k )
+			net.WriteString(k)
 			net.SendToServer()
 		end
 	end
+	return x
+end
+
+hook.Add("PostPlayerDraw", "RenderAbilityIndicator", function(Player)
+	local x = Player:LookupBone("ValveBiped.Bip01_Neck1")
+	if not x then return end
+	local pos, ang = Player:GetBonePosition(x)
+
+	cam.Wrap3D2D(function()
+		draw.WebImage( Emoji.Get(util.CRC((Player:SteamID64() or Player:UniqueID())) % #Emoji.Index), -8, -8, 16, 16)
+	end, pos - ang:Forward() * ( 10 + ( Player:GetVelocity():Length2D() > Player:GetRunSpeed() * 0.95 and 5 or 0) ), ang:SetPitch( -45 ):SetRoll( -90 ) + Angle(-135, 90, 0), 1.25)
+end)
+
+hook.Add("Lava.PopulateWidgetMenu", "CreateAbilitiesWidget", function( Context )
+	Context.NewWidget("Abilities", 733, CreateAbilitiesPanel )
 end)
