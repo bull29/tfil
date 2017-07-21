cd5528f074082c507a5aa5dfb6e07f87ab619835
Bull <thebull2140@gmail.com>
2017-07-21 17:49:43 -0400

    hippity hoppitus

---
+++ gamemode/core/abilities/hippityhoppity/hippityhoppity.lua
@@ -0,0 +1,20 @@
+
+
+hook.Add("GetFallDamage", "AvoidFallDamageHH", function(Player, Speed )
+	if Player:HasAbility("Hippity Hoppity") then
+		if not Player:Crouching() then
+			Player:SetVelocity( ( Player:GetAimVector() * 0.35 ):SetZ( 1.2 ) * ( Speed*0.75 ) )
+		end
+		return ( CalculateBaseFallDamage( Speed ) * 0.75 ):min( 15 )
+	end
+end)
+
+Abilities.Register("Hippity Hoppity", [[When you hit the ground
+	at a high velocity, you hop
+	extremely high, taking a flat maximum of 15
+	fall damage no matter the height. 
+	Similar to Skippy feet. Extremely useful for
+	vertical-oriented maps. 
+	Hold down crouch when landing to 
+	avoid hopping.]], CLIENT and Emoji.Get( 724 ) )
+
