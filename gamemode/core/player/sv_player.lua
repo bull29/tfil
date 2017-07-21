4d3a9e56871d8b6ee16f67238a76077202fd3aaf
Bull <thebull2140@gmail.com>
2017-07-21 10:57:39 -0400

    base function derives

--- gamemode/core/player/sv_player.lua
+++ gamemode/core/player/sv_player.lua
@@ -3,10 +3,15 @@ function GM:PlayerSpawn(Player)
 	hook.Call( "ChoosePlayerClass", GAMEMODE, Player )
 	player_manager.OnPlayerSpawn( Player )
 	player_manager.RunClass( Player, "Spawn" )
-	Player:Give("lava_fists")
+	hook.Call( "PlayerSetModel", GAMEMODE, Player )
+	hook.Call( "PlayerLoadout", GAMEMODE, Player )
 	hook.Call("Lava.PostPlayerSpawn", nil, Player )
 end
 
 function GM:ChoosePlayerClass( Player )
 	player_manager.SetPlayerClass( Player, "lava_default" )
+end
+
+function GM:PlayerLoadout( Player )
+	Player:Give("lava_fists")
 end
