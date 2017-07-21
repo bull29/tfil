29cb3e304ba3c62578ed3661f90de8cc272e1c4f
Bull <thebull2140@gmail.com>
2017-07-21 11:17:13 -0400

    null kicker fix

--- gamemode/core/abilities/climb/climbing_vm.lua
+++ gamemode/core/abilities/climb/climbing_vm.lua
@@ -37,7 +37,7 @@ local function ViewmodelPopup(self, View, Weapon, Player)
 	c.RenderOverride = function(self)
 		--if GetViewEntity() ~= LocalPlayer() or EyePos() ~= LocalPlayer():EyePos() then return end
 		local cQuer = Player:GetNW2String("$climbquery", "")
-
+		if not IsValid( View ) then return end
 		if cQuer ~= "" then
 			c:SetCycle(0.2)
 			Player.m_cEffect:SetNoDraw(false)
