9b69992e99019a5278680c3c1fab4bfb06240469
Bull <thebull2140@gmail.com>
2017-07-22 04:28:18 -0400

    lil widget to help the minimalists

---
+++ gamemode/core/misc/cl_screenie_widget.lua
@@ -0,0 +1,13 @@
+local m_DisableRenderHUD
+
+hook.Add("Lava.PopulateWidgetMenu", "CreateScreenshotPane", function( Context )
+	Context.NewWidget( "Screenshot Mode", 1413, function()
+		m_DisableRenderHUD = not m_DisableRenderHUD
+	end)
+end)
+
+hook.Add( "HUDShouldDraw", "ScreenshotPane", function( name )
+	if m_DisableRenderHUD and name == "CHudGMod" then
+		return false
+	end
+end)
