09aa790614a6ff806f7a57747c293cf40cf97d9e
Bull <thebull2140@gmail.com>
2017-07-28 16:42:34 -0400

    [hackily] added the proper round hooks clientside

--- gamemode/core/rounds/sh_rounds.lua
+++ gamemode/core/rounds/sh_rounds.lua
@@ -11,3 +11,25 @@ end
 function Rounds.IsState( query )
 	return Rounds.GetState():lower() == query:lower()
 end
+
+if CLIENT then
+	local OldState
+
+	hook.Add("Tick", "CallClientsideRoundHooks", function()
+		if not OldState then
+			OldState = Rounds.GetState()
+		end
+
+		if OldState ~= Rounds.GetState() then
+			if OldState == "Started" then
+				hook.Call("Lava.PostRound")
+			elseif OldState == "Ended" then
+				hook.Call("Lava.PreroundStart")
+			elseif OldState == "PreroundStart" then
+				hook.Call("Lava.RoundStart")
+			end
+			OldState = Rounds.GetState()
+		end
+	end)
+end
+	
