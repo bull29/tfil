6ccfcfa3ba89e307b7d885f8a77fe4a7939f048a
Bull <thebull2140@gmail.com>
2017-07-29 18:06:26 -0400

    Clientside Notification overhaul

--- gamemode/core/notifications/cl_notifications.lua
+++ gamemode/core/notifications/cl_notifications.lua
@@ -1,46 +1,80 @@
-local function CreateNotification(title, time, InitialColor, sound )
-	InitialColor = InitialColor or pColor()
-	MsgC( Color( 255, 0, 0 ), "\nNotification: " )
-	MsgC( InitialColor, "\n"..title:gsub("\t", "").."\n" )
-	surface.PlaySound( sound )
+local pColor = pColor
+local WebElements = WebElements
+local Color = Color
+local White = Color(255, 255, 255)
+local surface = surface
+local draw = draw
 
-	local CurrentNotification = InitializePanel("CurrentNotification", "DLabel")
-	CurrentNotification:Dock(FILL)
-	CurrentNotification:SetContentAlignment(9)
-	CurrentNotification:SetFont("lava_mutator_notifier")
-	CurrentNotification:SetPaintedManually(true)
-	CurrentNotification:SetTextColor(InitialColor)
-	CurrentNotification.Removal = CurTime() + time
-	CurrentNotification:SetText( title )
-	local OldVal = 2500
+hook.RunOnce("HUDPaint", function()
+	local t = InitializePanel("LavaNotificationHandler", "DPanel")
+	t:SetSize(ScrW(), ScrH() * 0.35)
+	t:Center()
+	t:SetVerticalPos(ScrH() / 100)
+	t.Paint = function(s, w, h)
+		for Index, Child in pairs(t:GetChildren()) do
+			if Child.m_BeingRemoved then continue end
+			Child.Index = Index - 1
+		end
 
-	CurrentNotification.PaintOver = function(s)
-		if s.Removal < CurTime() then
-			s.CurrentColor = s.CurrentColor or s:GetTextColor()
-			s.CurrentColor = s.CurrentColor:Lerp(Color(255, 255, 255), FrameTime() * 3)
-			s:SetTextColor(s.CurrentColor)
-			OldVal = OldVal:lerp(OldVal + 200)
-
-			if OldVal > 2500 then
-				hook.Remove("PostDrawTranslucentRenderables", "DrawNotifications")
-				s:Remove()
-			end
-		else
-			OldVal = OldVal:lerp(LocalPlayer():GetVelocity():Length() / 20)
+		if #s:GetChildren() == 0 then
+			s:Hide()
 		end
 	end
 
-	hook.Add("PostDrawTranslucentRenderables", "DrawNotifications", function()
-		cam.Start3D2D(EyeAngles():Right() * -ScrH()*0.1 + LocalPlayer():EyePos() + LocalPlayer():GetAimVector() * (50 + OldVal), Angle(0, -90 + EyeAngles().y, 90), 0.1)
-		CurrentNotification:PaintManual()
-		cam.End3D2D()
+	local function GetPos(CurrentNum)
+		return (CurrentNum) * (ScrH() / 20 + ScrH() / 200)
+	end
+
+	local function AddNotification(Name, Icon, Time, Sound)
+		if Sound then
+			surface.PlaySound( Sound )
+		end
+
+		Icon, Time = Icon or 2438, Time or 6
+		local x = t:Add("DLabel")
+		x:SetSize(FontFunctions.GetWide(Name, "lava_notification_font") + ScrH() / 20 * 2.1, ScrH() / 20)
+		x.RemovalTime = CurTime() + Time
+		x:CenterHorizontal()
+		x.Alpha = 0
+		x:SetFont("lava_notification_font")
+		x:SetTextColor(White)
+		x:SetTextInset(ScrW() / 40, 0)
+		x:SetContentAlignment(5)
+		x:SetText(Name)
+
+		x.Paint = function(self, w, h)
+			draw.RoundedBox(4, 0, 0, w, h, pColor():Alpha(self.Alpha))
+			draw.WebImage(Emoji.Get(Icon), h / 2, h / 2, h * 0.8, h * 0.8, nil, ( CurTime() * 3 ):sin() * 15)
+			draw.WebImage(Emoji.Get(Icon), w - h / 2, h / 2, h * 0.8, h * 0.8, nil, ( CurTime() * 3 ):sin() * 15)
+
+			if CurTime() > self.RemovalTime then
+				self.m_BeingRemoved = true
+				self:SetVerticalPos(self:GetVerticalPos() - 1)
+				self.Alpha = self.Alpha:lerp(0)
+			else
+				self.Alpha = self.Alpha:lerp(200)
+
+				if self:GetVerticalPos() > GetPos(self.Index or 0) then
+					self:SetVerticalPos(self:GetVerticalPos():Approach(GetPos(self.Index or 0), 1))
+				else
+					self:SetVerticalPos(GetPos(self.Index or 0))
+				end
+			end
+
+			if (self:GetVerticalPos() + h) < 2 then
+				self:Remove()
+			end
+		end
+	end
+
+	net.Receive("lava_notification", function()
+		local Table = net.ReadTable()
+		local String = net.ReadString()
+
+		AddNotification( String, Table.ICON, Table.TIME, Table.SOUND )
+
+		if IsValid( t ) then
+			t:Show()
+		end
 	end)
-end
-
-function GM.CreateNotification( title, time, color, sound )
-	CreateNotification( title, time, color, sound )
-end
-
-net.Receive("lava_notification",function()
-	GAMEMODE.CreateNotification( net.ReadString(), net.ReadInt( 32 ), pColor(), net.ReadString())
 end)
