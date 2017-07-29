f2b03c21eab41a25569ce5e15f7cb0f4d9bd25c6
Bull <thebull2140@gmail.com>
2017-07-29 17:49:03 -0400

    Fixed Mutators not ending correctly clientside

--- gamemode/core/mutators/sh_mutators.lua
+++ gamemode/core/mutators/sh_mutators.lua
@@ -1,7 +1,6 @@
 --[[
 	Round Mutators - Shit to make each and every round more interesting
 ]]
-
 local Mutators = Mutators or {}
 Mutators.Events = Mutators.Events or {}
 Mutators.LastActiveMutator = ""
@@ -15,55 +14,61 @@ function Mutators.RegisterNewEvent(name, desc, startfunc, endfunc)
 end
 
 function Mutators.StartEvent(event)
-	if hook.Call("Lava.MutatorStart", nil, event, Mutators.Events[ event ] ) == false then return end
-
-	if not Mutators.Events[ event ] then return end
-
+	debug.Trace()
+	if hook.Call("Lava.MutatorStart", nil, event, Mutators.Events[event]) == false then return end
+	if not Mutators.Events[event] then return end
 	Mutators.LastActiveMutator = event
-	Mutators.Events[ event ].startfn()
+	Mutators.Events[event].startfn()
 
 	if SERVER then
 		SetGlobalString("$activemutator", event)
 	end
-
 end
 
-function Mutators.RegisterHooks( eventname, tab )
+function Mutators.RegisterHooks(eventname, tab)
 	Mutators.Events[eventname] = Mutators.Events[eventname] or {}
 	Mutators.Events[eventname].hooks = tab
 	local HookIndex = 1
 
-	return function( func )
-		hook.Add( tab[ HookIndex ], "mutator_hook_" .. HookIndex, func )
+	return function(func)
+		hook.Add(tab[HookIndex], "mutator_hook_" .. HookIndex, func)
 		HookIndex = HookIndex + 1
-		return tab[ HookIndex ], eventname, "mutator_hook_" .. HookIndex, func
+
+		return tab[HookIndex], eventname, "mutator_hook_" .. HookIndex, func
 	end
 end
 
 function Mutators.EndEvent()
-	local tab = Mutators.Events[ Mutators.LastActiveMutator ]
-	if not tab then return end
+	local tab = Mutators.Events[Mutators.LastActiveMutator]
+
+	if not tab then
+		SetGlobalString("$activemutator", "")
+		Mutators.LastActiveMutator = nil
+		return
+	end
+
 	if tab.endfn then
 		tab.endfn()
 	end
 
-	if Mutators.Events[ Mutators.LastActiveMutator ] and Mutators.Events[ Mutators.LastActiveMutator ].hooks then
-		for Index, Hook in pairs( Mutators.Events[ Mutators.LastActiveMutator ].hooks ) do
-			hook.Remove( Hook, "mutator_hook_" .. Index )
+	if Mutators.Events[Mutators.LastActiveMutator] and Mutators.Events[Mutators.LastActiveMutator].hooks then
+		for Index, Hook in pairs(Mutators.Events[Mutators.LastActiveMutator].hooks) do
+			hook.Remove(Hook, "mutator_hook_" .. Index)
 		end
 	end
 
-	SetGlobalString("$activemutator", "" )
+	SetGlobalString("$activemutator", "")
 	Mutators.LastActiveMutator = nil
 end
 
-function Mutators.IsActive( name )
+function Mutators.IsActive(name)
 	if not name then return GetGlobalString("$activemutator") ~= "" end
+
 	return GetGlobalString("$activemutator") == name
 end
 
 function Mutators.GetActive()
-	return GetGlobalString("$activemutator", false )
+	return GetGlobalString("$activemutator", false)
 end
 
 hook.RunOnce("HUDPaint", function()
@@ -78,22 +83,20 @@ hook.Add("Lava.PostRound", "EndMutator", function()
 	end
 end)
 
-function Mutators.GetRandomPlayerForEvent( event )
-	if not hook.Call( "Lava.ChooseRandomPlayerForEvent", nil, event ) then
-		return table.Random( player.GetAll() )
-	end
+function Mutators.GetRandomPlayerForEvent(event)
+	if not hook.Call("Lava.ChooseRandomPlayerForEvent", nil, event) then return table.Random(player.GetAll()) end
 end
 
-function Mutators.DesignateSpecialPlayer( Player )
-	SetGlobalEntity("$currentmutantplayer", Player )
+function Mutators.DesignateSpecialPlayer(Player)
+	SetGlobalEntity("$currentmutantplayer", Player)
 end
 
 function Mutators.GetSpecialPlayer()
-	return IsValid( GetGlobalEntity("$currentmutantplayer", NULL ) ) and GetGlobalEntity("$currentmutantplayer")
+	return IsValid(GetGlobalEntity("$currentmutantplayer", NULL)) and GetGlobalEntity("$currentmutantplayer")
 end
 
 function Mutators.ClearSpecialPlayer()
-	SetGlobalEntity("$currentmutantplayer", NULL )
+	SetGlobalEntity("$currentmutantplayer", NULL)
 end
 
 _G.Mutators = Mutators
