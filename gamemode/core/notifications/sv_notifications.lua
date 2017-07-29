9959225d99dd3b188b929f2bf7f0e8febd945a78
Bull <thebull2140@gmail.com>
2017-07-29 17:50:21 -0400

    Barebones notification presetting

--- gamemode/core/notifications/sv_notifications.lua
+++ gamemode/core/notifications/sv_notifications.lua
@@ -1,13 +1,33 @@
+local Notification = {}
+local net = net
+local util = util
+
+Notification.Presets = {}
+
 util.AddNetworkString("lava_notification")
 
-function GM.CreateNotification( title, time, player, sound )
+function Notification.Create( Text, Table, Player )
 	net.Start("lava_notification")
-	net.WriteString( title )
-	net.WriteInt( time, 32 )
-	net.WriteString( sound or "ambient/alarms/warningbell1.wav")
-	if player then
-		net.Send( player )
-	else
-		net.Broadcast()
-	end
+	net.WriteTable( Table )
+	net.WriteString( Text )
+	net.Send( Player or player.GetAll() )
 end
+
+function Notification.SendType( Type, Text, Player )
+	net.Start("lava_notification")
+	net.WriteTable( Notification.Presets[ Type ] )
+	net.WriteString( Text )
+	net.Send( Player or player.GetAll() )
+end
+
+function Notification.CreateType( TypeName, Data )
+	Notification.Presets[ TypeName ] = Data
+end
+
+Notification.CreateType( "General", {
+	SOUND = "ambient/water/drip2.wav",
+	TIME = 5,
+	ICON = 2438
+})
+
+_G.Notification = Notification
