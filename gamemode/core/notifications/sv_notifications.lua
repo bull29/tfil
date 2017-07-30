45f30a709f00bead4b1020807e407cecfca1ae53
Bull <thebull2140@gmail.com>
2017-07-30 03:06:39 -0400

    Some default notification events

--- gamemode/core/notifications/sv_notifications.lua
+++ gamemode/core/notifications/sv_notifications.lua
@@ -1,37 +1,66 @@
 local Notification = {}
 local net = net
 local util = util
-
 Notification.Presets = {}
-
 util.AddNetworkString("lava_notification")
 
-function Notification.Create( Text, Table, Player )
-	if hook.Call("Lava.NotificationDispatch", nil, Text, Table, Player ) == false then return end
-
+function Notification.Create(Text, Table, Player)
+	if hook.Call("Lava.NotificationDispatch", nil, Text, Table, Player) == false then return end
 	net.Start("lava_notification")
-	net.WriteTable( Table )
-	net.WriteString( Text )
-	net.Send( Player or player.GetAll() )
+	net.WriteTable(Table)
+	net.WriteString(Text)
+	net.Send(Player or player.GetAll())
 end
 
-function Notification.SendType( Type, Text, Player )
-	if hook.Call("Lava.NotificationDispatch", nil, Text, Notification.Presets[ Type ], Player ) == false then return end
-
+function Notification.SendType(Type, Text, Player)
+	if hook.Call("Lava.NotificationDispatch", nil, Text, Notification.Presets[Type], Player) == false then return end
 	net.Start("lava_notification")
-	net.WriteTable( Notification.Presets[ Type ] )
-	net.WriteString( Text )
-	net.Send( Player or player.GetAll() )
+	net.WriteTable(Notification.Presets[Type])
+	net.WriteString(Text)
+	net.Send(Player or player.GetAll())
 end
 
-function Notification.CreateType( TypeName, Data )
-	Notification.Presets[ TypeName ] = Data
+function Notification.CreateType(TypeName, Data)
+	Notification.Presets[TypeName] = Data
 end
 
-Notification.CreateType( "General", {
+Notification.CreateType("General", {
 	SOUND = "ambient/water/drip2.wav",
 	TIME = 5,
 	ICON = 2438
 })
+
+Notification.CreateType("Join", {
+	SOUND = "garrysmod/save_load4.wav",
+	TIME = 6,
+	ICON = 347
+})
+
+Notification.CreateType("Enter", {
+	SOUND = "garrysmod/save_load1.wav",
+	TIME = 6,
+	ICON = 348
+})
+
+Notification.CreateType("Leave", {
+	SOUND = "garrysmod/save_load2.wav",
+	TIME = 6,
+	ICON = 346
+})
+
+gameevent.Listen("player_connect")
+gameevent.Listen("player_disconnect")
+
+hook.Add("player_connect", "JoinNotif", function(data)
+	Notification.SendType("Join", data.name .. " has connected to the server!")
+end)
+
+hook.Add("player_disconnect", "JoinNotif", function(data)
+	Notification.SendType("Leave", data.name .. " has left the server. ( " .. data.reason .. " )")
+end)
+
+hook.Add("PlayerInitialSpawn", "EnteredServer", function(Player)
+	Notification.SendType("Enter", Player:Nick() .. " has entered the server!")
+end)
 
 _G.Notification = Notification
