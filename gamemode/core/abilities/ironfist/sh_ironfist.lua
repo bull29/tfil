62d536a3370d2d4fcb223ea396f5472672870c62
Bull <thebull2140@gmail.com>
2017-07-31 18:01:45 -0400

    buffed iron fist but added con

--- gamemode/core/abilities/ironfist/sh_ironfist.lua
+++ gamemode/core/abilities/ironfist/sh_ironfist.lua
@@ -2,9 +2,16 @@
 
 hook.Add("Lava.PlayerPushPlayer", "Iron Fist", function( Attacker, Victim )
 	if Attacker:HasAbility("Iron Fist") and not Victim:HasAbility("Stoneman") then
-		Victim:SetVelocity(Attacker:GetForward():SetZ(Victim:OnGround() and 0.5 or -0.5) * 2000)
+		Victim:SetVelocity(Attacker:GetForward():SetZ(Victim:OnGround() and 0.5 or -0.5) * 3000)
 		return true
 	end
 end)
 
-Abilities.Register("Iron Fist", [[Because of vigorous use of both your hands, your punches cause players to go flying towards wherever you happen to be aiming, much stronger than the right-handed normies.]], 1990 )
+hook.Add("Lava.PostPlayerSpawn", "Iron Fist", function( Player )
+	if Player:HasAbility("Iron Fist") then
+		Player:SetRunSpeed( Player:GetRunSpeed() * 0.8 )
+		Player:SetWalkSpeed( Player:GetWalkSpeed() * 0.8 )
+	end
+end)
+
+Abilities.Register("Iron Fist", [[Because of vigorous use of both your hands, your punches cause players to go flying towards wherever you happen to be aiming, much stronger than the right-handed normies. Unfortunately, you move slower than everyone.]], 1990 )
