4d6cd96fef8abe00f59080f94e3cf19f8f478388
Bull <thebull2140@gmail.com>
2017-07-31 18:11:53 -0400

    Emoji Whisperer :eyes:

---
+++ gamemode/core/abilities/psychic/sh_emojiwhisperer.lua
@@ -0,0 +1,8 @@
+
+hook.Add("Lava.ESPOverride", "EmojiMoviebySony", function( Player )
+	if Player:HasAbility("Emoji Whisperer") then
+		return 99
+	end
+end)
+
+Abilities.Register("Emoji Whisperer", [[Due to your binge watching of the Emoji Movie, you have developed a spiritual connection with Emojis and can sense them for an infinite amount of time.]], 734 )
