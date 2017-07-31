cd50e9c3cc0792b747bdcff9faacc02192da7fac
Bull <thebull2140@gmail.com>
2017-07-31 03:19:49 -0400

    Flametag

---
+++ gamemode/core/mutators/mutators/sh_flametag.lua
@@ -0,0 +1,48 @@
+local AddHook = Mutators.RegisterHooks("Flametag", {
+	"PlayerShouldTakeDamage",
+	"Think",
+	"PlayerTick"
+})
+
+Mutators.RegisterNewEvent("Flametag", "A Randomly chosen player is flaming. If he comes near you, you catch on fire too. Just give him a hug!", function()
+	if SERVER then
+		local Player = Mutators.GetRandomPlayerForEvent("Flametag")
+		if not Player then return end
+		Mutators.DesignateSpecialPlayer(Player)
+
+		if Mutators.GetSpecialPlayer() then
+			local sPlayer = Mutators.GetSpecialPlayer()
+			sPlayer:SetModel("models/player/charple.mdl")
+			sPlayer:SetWalkSpeed( 100 )
+			sPlayer:SetRunSpeed( 200 )
+			if SERVER then
+				sPlayer:Ignite(500, 128)
+				sPlayer.PreferedAbility = sPlayer:GetAbility()
+				sPlayer:SetAbility("$none")
+			end
+
+			AddHook(function(Player, Attacker)
+				if Player == sPlayer and IsValid(Attacker) and Attacker:GetClass() == "entityflame" and Attacker:GetPos().z > Lava.GetLevel() then return false end
+			end)
+			AddHook(function() end)
+		end
+	end
+	if CLIENT then
+		AddHook( function() end)
+		AddHook( function()
+			if LocalPlayer() == Mutators.GetSpecialPlayer() then
+				_ontp = true
+			end
+		end)
+	end
+	AddHook( function( Player )
+		local Special = Mutators.GetSpecialPlayer()
+		if Player ~= Special and IsValid( Special ) then
+			if Player:GetPos():Distance( Special:GetPos() ) < 75 then
+				Player:Ignite( 0.1, 0 )
+			end
+		end
+	end)
+end, function()
+	Mutators.ClearSpecialPlayer()
+end)
