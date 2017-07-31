9ed98f4a58f7423d00bab950d6816627cde52148
Bull <thebull2140@gmail.com>
2017-07-30 20:41:15 -0400

    serverlogging + mutators basics

--- gamemode/core/notifications/sv_notifications.lua
+++ gamemode/core/notifications/sv_notifications.lua
@@ -1,23 +1,26 @@
 local Notification = {}
 local net = net
+local ServerLog = ServerLog
 local util = util
 Notification.Presets = {}
 util.AddNetworkString("lava_notification")
 
 function Notification.Create(Text, Table, Player)
 	if hook.Call("Lava.NotificationDispatch", nil, Text, Table, Player) == false then return end
+	ServerLog( Text .. "\n" )
 	net.Start("lava_notification")
 	net.WriteTable(Table)
 	net.WriteString(Text)
-	net.Send(Player or player.GetAll())
+	net.Send(IsValid( Player ) and Player or player.GetAll())
 end
 
 function Notification.SendType(Type, Text, Player)
 	if hook.Call("Lava.NotificationDispatch", nil, Text, Notification.Presets[Type], Player) == false then return end
+	ServerLog( Text .. "\n" )
 	net.Start("lava_notification")
 	net.WriteTable(Notification.Presets[Type])
 	net.WriteString(Text)
-	net.Send(Player or player.GetAll())
+	net.Send(IsValid( Player ) and Player or player.GetAll())
 end
 
 function Notification.CreateType(TypeName, Data)
@@ -52,6 +55,12 @@ Notification.CreateType("Winner", {
 	SOUND = "garrysmod/save_load3.wav",
 	TIME = 10,
 	ICON = 2188
+})
+
+Notification.CreateType("Mutator", {
+	SOUND = "npc/scanner/scanner_siren1.wav",
+	TIME = 10,
+	ICON = 2
 })
 
 gameevent.Listen("player_connect")
