81ef433f04b39d1bc169b5c2d0c53d946c28146d
Bull <thebull2140@gmail.com>
2017-08-02 16:45:27 -0400

    fixed targetID on ragdolls

--- gamemode/core/hud/cl_targetid.lua
+++ gamemode/core/hud/cl_targetid.lua
@@ -11,8 +11,8 @@ hook.Add("PostDrawTranslucentRenderables", "ThisThing", function()
 	x = LocalPlayer():EyeEntity()
 
 	if x:IsValid() and (x:IsPlayer() or x.m_Player) then
-		x = x.m_Player or x
 		bPos = x:GetBonePosition(x:LookupBone("ValveBiped.Bip01_Head1")) + Vector(0, 0, 15)
+		x = x.m_Player or x
 
 		cam.Wrap3D2D(function()
 			local Wide, Tall = FontFunctions.GetSize(x:Nick(), "ChatFont")
