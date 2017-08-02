14e7ad09e274afb0e58801218d22e793eeb58e98
Bull <thebull2140@gmail.com>
2017-08-02 15:27:56 -0400

    refactored linux fix as to not redeclare a function every frame

--- gamemode/core/lava/cl_lava.lua
+++ gamemode/core/lava/cl_lava.lua
@@ -50,7 +50,7 @@ hook.Add("PostDrawTranslucentRenderables", "DrawLava", function(a, b)
 	local LavaLevel = v:SetZ(SmoothLevel)
 	local Ang = Angle(0, CurTime(), 0)
 
-	local function drawLava()
+	render.Clip(ClipTab, function()
 		local x = 220 + (CurTime():sin() * 35):abs()
 		surface.SetDrawColor(x, x, x)
 		surface.SetMaterial(draw.fetch_asset(LavaTexture, "noclamp"))
@@ -66,13 +66,7 @@ hook.Add("PostDrawTranslucentRenderables", "DrawLava", function(a, b)
 				end, LavaLevel, Ang + Angle( 180, 0, 0 ), 1)
 			end
 		end
-	end
-
-	if system.IsWindows() then
-		render.Clip(ClipTab, drawLava)
-	else
-		drawLava()
-	end
+	end)
 
 	if b then
 		cam.Wrap3D2D(function()
