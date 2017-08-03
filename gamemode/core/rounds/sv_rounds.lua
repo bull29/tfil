3a27433f59838b7a71d99176f888ef34fda20e96
Bull <thebull2140@gmail.com>
2017-08-02 23:06:45 -0400

    fixed -1 hook call to map switch

--- gamemode/core/rounds/sv_rounds.lua
+++ gamemode/core/rounds/sv_rounds.lua
@@ -21,7 +21,7 @@ end
 
 
 function Rounds.Preround()
-	if NextMapTime > Config.GetMapSwitchTime() then
+	if NextMapTime >= Config.GetMapSwitchTime() then
 		if hook.Call("Lava.GetNextMap") == nil then
 			game.LoadNextMap()
 		end
