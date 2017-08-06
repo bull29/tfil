6870e70900bac4ae5e500cf990386a96760ee22a
Bull <thebull2140@gmail.com>
2017-08-06 03:34:41 -0400

    Removed depreciated unstuck widget

--- gamemode/core/unstuck/cl_unstuck.lua
+++
@@ -1,8 +0,0 @@
-
-
-hook.Add("Lava.PopulateWidgetMenu", "CreateUnstuckWidet", function( Context )
-	Context.NewWidget( "Unstuck", 2618, function()
-		net.Start("lava_unstuck")
-		net.SendToServer()
-	end)
-end)
