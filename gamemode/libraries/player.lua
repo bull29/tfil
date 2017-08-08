bdbe388a0ca3618c38764e43483dd1acc8f3c03e
Bull <thebull2140@gmail.com>
2017-08-08 00:39:09 -0400

    Antistuck now ignores ladders, casper

--- gamemode/libraries/player.lua
+++ gamemode/libraries/player.lua
@@ -24,7 +24,7 @@ function Player:CheckHullCollision()
 		maxs = self:OBBMaxs(),
 	}
 
-	if tR.Entity and (tR.Entity:IsWorld() or IsValid(tR.Entity) or tR.StartSolid or tR.AllSolid ) then
+	if self:GetMoveType() ~= 9 and not self:HasAbility("Casper") and tR.Entity and (tR.Entity:IsWorld() or IsValid(tR.Entity) or tR.StartSolid or tR.AllSolid ) then
 		return tR
 	end
 
