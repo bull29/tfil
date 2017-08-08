dc087b875661b32dfa359b1b0135648ff6d7f186
Bull <thebull2140@gmail.com>
2017-08-07 22:28:29 -0400

    Ironfist now affects stoneman

--- gamemode/core/abilities/ironfist/sh_ironfist.lua
+++ gamemode/core/abilities/ironfist/sh_ironfist.lua
@@ -1,7 +1,7 @@
 
 
 hook.Add("Lava.PlayerPushPlayer", "Iron Fist", function( Attacker, Victim )
-	if Attacker:HasAbility("Iron Fist") and not Victim:HasAbility("Stoneman") then
+	if Attacker:HasAbility("Iron Fist") then
 		Victim:SetVelocity(Attacker:GetForward():SetZ(-0.2) * 1500)
 		return true
 	end
