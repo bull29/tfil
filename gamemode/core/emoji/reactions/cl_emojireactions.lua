57812f063970fbbe84d35324ccd5b870d91f7701
Bull <thebull2140@gmail.com>
2017-08-13 17:10:53 -0400

    New: Emoji Reactions now chain and compound! (6)

--- gamemode/core/emoji/reactions/cl_emojireactions.lua
+++ gamemode/core/emoji/reactions/cl_emojireactions.lua
@@ -16,7 +16,7 @@ hook.RunOnce("HUDPaint", function()
 			if not Player:Alive() or (Player == LocalPlayer() and not Player:ShouldDrawLocalPlayer()) then continue end
 
 			Player:OnBoneExisting("ValveBiped.Bip01_Head1", function(bid)
-				if Player.m_CurrentEmoji then
+				if Player.m_CurrentEmojis and Player.m_EmojiShowTime then
 					PlayerAlphas[Player] = PlayerAlphas[Player] or 0
 
 					if Player.m_EmojiShowTime > CurTime() then
@@ -30,7 +30,12 @@ hook.RunOnce("HUDPaint", function()
 
 						cam.Wrap3D2D(function()
 							draw.WebImage(WebElements.SpeechBubble, 0, -40, 40, 40, (Player:PlayerColor() - 50):Alpha(PlayerAlphas[Player]))
-							draw.WebImage(Emoji.Get(Player.m_CurrentEmoji), 20, -25, 10, 10, White:Alpha(PlayerAlphas[Player]), 0)
+							local Amount = #Player.m_CurrentEmojis
+
+							for i = 1, Amount do
+								local Size = (20 / Amount):min(10):max(5)
+								draw.WebImage(Emoji.Get(Player.m_CurrentEmojis[i]), 20 + (i - 1) * Size - (Amount - 1) * Size / 2, -25, Size, Size, White:Alpha(PlayerAlphas[Player]), 0)
+							end
 						end, Player:GetBonePosition(bid), Angle(0, 270 + EyeAngles().y, 90), 1)
 
 						if Player.m_EmojiPreText then
@@ -49,6 +54,7 @@ hook.RunOnce("HUDPaint", function()
 					else
 						Player.m_EmojiPreText = nil
 						Player.m_EmojiPostText = nil
+						Player.m_CurrentEmojis = {}
 					end
 				end
 			end)
@@ -56,29 +62,41 @@ hook.RunOnce("HUDPaint", function()
 	end)
 
 	hook.Add("OnPlayerChat", "EmojiInterperate", function(Player, Text)
-		--if (not Player.m_EmojiShowTime or Player.m_EmojiShowTime < CurTime()) then
-			if Text:match("^%$%d+$") then
-				local int = tonumber((Text:match("^%$%d+"):gsub("%D", "")))
-				if int > 2661 or int < 0 then return end
-				Player.m_CurrentEmoji = int
-				Player.m_EmojiShowTime = CurTime() + 10
-				Player:EmitSound("garrysmod/balloon_pop_cute.wav")
-				return true
+		if Text:match("^%$%d+$") then
+			local int = tonumber((Text:match("^%$%d+"):gsub("%D", "")))
+			if int > 2661 or int < 0 then return end
+			Player.m_CurrentEmojis = Player.m_CurrentEmojis or {}
+			table.insert(Player.m_CurrentEmojis, int)
+
+			if #Player.m_CurrentEmojis > 6 then
+				table.remove(Player.m_CurrentEmojis, 1)
 			end
 
-			if Text:match("%{%$%d+%}") then
-				local data = Text:match("%{%$%d+%}")
-				local EmojiNum = data:gsub("[%{%$%}]", "")
-				local Prepend = Text:Split(data)[1]
-				local Postend = Text:Split(data)[2]
-				Player.m_CurrentEmoji = tonumber(EmojiNum)
-				Player.m_EmojiShowTime = CurTime() + 10
-				Player.m_EmojiPreText = Prepend
-				Player.m_EmojiPostText = Postend:gsub("^%s", "")
-				Player:EmitSound("garrysmod/balloon_pop_cute.wav")
-				return true
+			Player.m_EmojiShowTime = CurTime() + 7
+			Player:EmitSound("garrysmod/balloon_pop_cute.wav")
+
+			return true
+		end
+
+		if Text:match("%{%$%d+%}") then
+			local data = Text:match("%{%$%d+%}")
+			local EmojiNum = data:gsub("[%{%$%}]", "")
+			local Prepend = Text:Split(data)[1]
+			local Postend = Text:Split(data)[2]
+			Player.m_CurrentEmojis = Player.m_CurrentEmojis or {}
+			table.insert(Player.m_CurrentEmojis, int)
+
+			if #Player.m_CurrentEmojis > 6 then
+				table.remove(Player.m_CurrentEmojis, 1)
 			end
-	--	end
+
+			Player.m_EmojiShowTime = CurTime() + 10
+			Player.m_EmojiPreText = Prepend
+			Player.m_EmojiPostText = Postend:gsub("^%s", "")
+			Player:EmitSound("garrysmod/balloon_pop_cute.wav")
+
+			return true
+		end
 
 		local x = Text:match("^%$%a+%=%d+$")
 
@@ -105,6 +123,7 @@ hook.RunOnce("HUDPaint", function()
 	end)
 
 	local cNum
+
 	hook.Add("ChatTextChanged", "EmojiBindLookup", function(Text)
 		if Text:sub(1, 1) == "$" and EmojiDictionary[Text:gsub("%$", "")] then
 			gui.InternalKeyCodeTyped(KEY_TAB)
@@ -114,8 +133,9 @@ hook.RunOnce("HUDPaint", function()
 			gui.InternalKeyCodeTyped(KEY_TAB)
 		end
 
-		if Text:sub(1,1) == "$" and Text:match("%$%d+$") then
-			cNum = tonumber( (Text:gsub("%D", "" )) )
+		if Text:sub(1, 1) == "$" and Text:match("%$%d+$") then
+			cNum = tonumber((Text:gsub("%D", "")))
+
 			if cNum > #Emoji.Index then
 				cNum = nil
 			end
@@ -128,22 +148,28 @@ hook.RunOnce("HUDPaint", function()
 		if not cNum then return end
 		local x, y = chat.GetChatBoxPos()
 		local sx, sy = chat.GetChatBoxSize()
-
-		draw.WebImage( Emoji.Get( cNum ), x + sx + WebElements.Edge * 2, y + sy - ScrH()/25 - WebElements.Edge, ScrH()/25, ScrH()/25 )
+		draw.WebImage(Emoji.Get(cNum), x + sx + WebElements.Edge * 2, y + sy - ScrH() / 25 - WebElements.Edge, ScrH() / 25, ScrH() / 25)
 	end)
 
 	local LocalPlayerAlpha = 0
 
 	hook.Add("HUDPaint", "RenderLocalPlayer", function()
-		if LocalPlayer():Alive() and LocalPlayer().m_CurrentEmoji and LocalPlayer().m_EmojiShowTime > CurTime() and not LocalPlayer():ShouldDrawLocalPlayer() then
+		if LocalPlayer():Alive() and LocalPlayer().m_CurrentEmojis and LocalPlayer().m_EmojiShowTime and LocalPlayer().m_EmojiShowTime > CurTime() and not LocalPlayer():ShouldDrawLocalPlayer() then
 			LocalPlayerAlpha = LocalPlayerAlpha:lerp(255)
 		elseif LocalPlayerAlpha > 1 then
 			LocalPlayerAlpha = LocalPlayerAlpha:lerp(0)
+		elseif LocalPlayerAlpha < 1 and not LocalPlayer():ShouldDrawLocalPlayer() then
+			LocalPlayer().m_CurrentEmojis = {}
 		end
 
-		if LocalPlayerAlpha > 1 and LocalPlayer():Alive() and LocalPlayer().m_CurrentEmoji then
+		if LocalPlayerAlpha > 1 and LocalPlayer():Alive() and LocalPlayer().m_CurrentEmojis then
 			draw.WebImage(WebElements.SpeechBubble, ScrW() * 0.25, ScrH() - ScrH() / 8, ScrH() / 4, ScrH() / 4, (pColor() - 50):Alpha(LocalPlayerAlpha), 0)
-			draw.WebImage(Emoji.Get(LocalPlayer().m_CurrentEmoji), ScrW() * 0.25, ScrH() - ScrH() / 32 - ScrH() / 8, ScrH() / 16, ScrH() / 16, White:Alpha(LocalPlayerAlpha), CurTime():sin() * -15)
+			local Amount = #LocalPlayer().m_CurrentEmojis
+
+			for i = 1, Amount do
+				local Size = (ScrW() * 0.05 / Amount):min(ScrW() * 0.5):max(ScrH() / 32)
+				draw.WebImage(Emoji.Get(LocalPlayer().m_CurrentEmojis[i]), ScrW() * 0.25 + (i - 1) * Size - (Amount - 1) * Size / 2, ScrH() - ScrH() / 32 - ScrH() / 8, Size, Size, White:Alpha(LocalPlayerAlpha), 0)
+			end
 		end
 	end)
 end)
