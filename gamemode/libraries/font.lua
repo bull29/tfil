f79f2c2597e4184f313f68fd59cedbf1755bb0e7
Bull <thebull2140@gmail.com>
2017-08-13 03:02:48 -0400

    GenerateFont for dynamically scaled text

--- gamemode/libraries/font.lua
+++ gamemode/libraries/font.lua
@@ -1,5 +1,7 @@
-FontFunctions = {}
+local FontFunctions = {}
 local CurrentFont = "ChatFont"
+local TemporaryFonts = {}
+local GeneratedFonts = {}
 
 function FontFunctions.SetActiveFont(font)
 	CurrentFont = font
@@ -23,6 +25,7 @@ end
 function FontFunctions.GetSize(text, font)
 	CurrentFont = font or CurrentFont
 	surface.SetFont(CurrentFont)
+
 	return surface.GetTextSize(text)
 end
 
@@ -31,3 +34,26 @@ function FontFunctions.GetDesiredHeight(text, width, font)
 
 	return width * FontFunctions.GetTall(text) / FontFunctions.GetWide(text)
 end
+
+function FontFunctions.GenerateFont(text, width, font)
+	if GeneratedFonts[ text .. width .. font ] then
+		return GeneratedFonts[ text .. width .. font ]
+	end
+
+	if not TemporaryFonts[ font ] then
+		surface.CreateFont( "temporary_font_" .. font, {
+			font = font
+		})
+		TemporaryFonts[ font ] = "temporary_font_" .. font
+	end
+
+	surface.CreateFont("lava_generated_font_" .. width .. "_" .. font:lower(), {
+		font = font,
+		size = FontFunctions.GetDesiredHeight(text, width, TemporaryFonts[ font ])
+	})
+
+	GeneratedFonts[ text .. width .. font ] = "lava_generated_font_" .. width .. "_" .. font:lower()
+	return GeneratedFonts[ text .. width .. font ]
+end
+
+_G.FontFunctions = FontFunctions
