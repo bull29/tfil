164abc0a5f8e65fb430fd2a82789e2db1ffd504c
Bull <thebull2140@gmail.com>
2018-08-25 16:43:24 -0400

    Removed Useless Emoji Whisperer ability ( For now )

--- gamemode/core/abilities/psychic/sh_emojiwhisperer.lua
+++
@@ -1,8 +0,0 @@
-
-hook.Add("Lava.ESPOverride", "EmojiMoviebySony", function( Player )
-	if Player:HasAbility("Emoji Whisperer") then
-		return 99
-	end
-end)
-
-Abilities.Register("Emoji Whisperer", [[Due to your binge watching of the Emoji Movie, you have developed a spiritual connection with Emojis and can sense them for an infinite amount of time.]], "1f441" )
